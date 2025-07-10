fe7cf2e3db011be401a3f723ab43bac11ed5fa76
akashkumar491 <visitak7@gmail.com>
2025-07-10 17:52:24 +0530

    imported css

--- src/embed.js
+++ src/embed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ChatWidget from './ChatWidget';
+import './ChatWidget.css'; 
 
 window.renderChatWidget = function ({ propertyId }) {
   const container = document.createElement('div');
